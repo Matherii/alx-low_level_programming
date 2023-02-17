2558fa1d20ae072512a85daed1da8a01988baebd
Matherii <mkdir>
2023-02-16 18:16:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main()
 {
-	char prog[] =""Programming is like building a multilingual language";
-	puts (prog);
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return 0;
 }
