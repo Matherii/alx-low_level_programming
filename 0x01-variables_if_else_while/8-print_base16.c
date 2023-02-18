4b0523af2d5a623751a1c1a5c6f3d3c39d6ca67a
Matherii <mkdir>
2023-02-17 19:29:37 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+        int p;
+
+        char z;
+
+        z = 'a';
+	p = 0;
+	while
+		(p < 10) {
+			putchar(p + '0');
+			p++;
+		}
+	while
+		(z <= 'f') {
+			putchar(z);
+			z++;
+		}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base_16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int p;
+
+	char z;
+
+	p = 0;
+	
+	z = 'a';
+
+	for (p = 0; p <= 0; p++)
+		putchar(p);
+	for (z = 'a'; z <= 'f'; z++)
+		putchar(z);
+	putchar('\n');
+	return (0);
+}
+
