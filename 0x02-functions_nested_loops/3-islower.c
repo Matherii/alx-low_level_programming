0a25a1e8e34061b36fdec44d37bbd0f001589222
Matherii <mkdir>
2023-02-22 12:01:12 -0800

    checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
 /**
- * _islower - checksfor lowercase
+ * _islower - checks for lowercase character
  * @c: The character to be checked
  * Return 0 for lowercase character, 1 for anything else
  */
 
 int _islower(int c)
-/**
- * Return: value for _islower is 1
- */
 
 {
 	if (c >= 97 && c <= 122)
