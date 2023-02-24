8fceb04c3fd8ca4325de56087f418c84d411d49e
Matherii <mkdir>
2023-02-24 15:59:32 -0800

    task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,6 +4,7 @@
  * main - tests function that prints if integer is positive or negative
  * return: 0
  */
+
 int main(void)
 
 {
@@ -12,5 +13,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
